99daaafd48644d87150087f8c3008c50fa33024a
Marcos Aurélio Carvalho <116835998+marcossaurelio@users.noreply.github.com>
2022-12-01 17:12:30 -0300

    Add files via upload

--- matrizes_main.py
+++ matrizes_main.py
@@ -1,13 +1,23 @@
 linhas_a = int(input('Quantidade de linhas do vetor A: '))
 colunas_a = int(input('Quantidade de colunas do vetor A: '))
-#linhas_b = int(input('Quantidade de linhas do vetor B: '))
-#colunas_b = int(input('Quantidade de colunas do vetor B: '))
 
-a = [[None]*colunas_a]*linhas_a
-#b = [[None]*colunas_b]*linhas_b
+a = []
 
-for i in range(linhas_a):
-    for j in range(colunas_a):
-        a[i][j] = int(input())
-        
-bp=1
+for l in range(linhas_a):
+    linha = []
+    for c in range(colunas_a):
+        linha.append(float(input(f'Digite o elemento a[{l}][{c}]: ')))
+    a.append(linha)
+    
+    
+linhas_b = int(input('Quantidade de linhas do vetor B: '))
+colunas_b = int(input('Quantidade de colunas do vetor B: '))
+
+b = []
+
+for l in range(linhas_b):
+    linha = []
+    for c in range(colunas_b):
+        linha.append(float(input(f'Digite o elemento b[{l}][{c}]: ')))
+    b.append(linha)
+    

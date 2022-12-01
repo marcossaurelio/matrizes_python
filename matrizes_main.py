93635e743e023774514c1c141ea268de255188d7
Marcos Aurélio Carvalho <116835998+marcossaurelio@users.noreply.github.com>
2022-12-01 13:18:21 -0300

    Add files via upload

---
+++ matrizes_main.py
@@ -0,0 +1,13 @@
+linhas_a = int(input('Quantidade de linhas do vetor A: '))
+colunas_a = int(input('Quantidade de colunas do vetor A: '))
+#linhas_b = int(input('Quantidade de linhas do vetor B: '))
+#colunas_b = int(input('Quantidade de colunas do vetor B: '))
+
+a = [[None]*colunas_a]*linhas_a
+#b = [[None]*colunas_b]*linhas_b
+
+for i in range(linhas_a):
+    for j in range(colunas_a):
+        a[i][j] = float(input())
+        
+bp=1
